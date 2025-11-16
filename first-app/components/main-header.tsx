2d4309b397dd01cefe668bedd73144dfa4565664
lapeko <lapeko88@gmail.com>
2025-11-16 22:39:56 +0100

    refactor background

--- first-app/components/main-header.tsx
+++ first-app/components/main-header.tsx
@@ -2,25 +2,29 @@ import Link from "next/link";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
 
+import MainHeaderBackgroud from "./main-header-background";
 import c from "./main-header.module.css";
 
 const MainHeader = () => (
-  <header className={c.header}>
-    <Link className={c.logo} href="/">
-      <Image src={logo} alt="a plate with food" priority />
-      NextLevel food
-    </Link>
-    <nav className={c.nav}>
-      <ul>
-        <li>
-          <Link href="/meals">Browse meals</Link>
-        </li>
-        <li>
-          <Link href="/community">Foodies Community</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <>
+    <MainHeaderBackgroud />
+    <header className={c.header}>
+      <Link className={c.logo} href="/">
+        <Image src={logo} alt="a plate with food" priority />
+        NextLevel food
+      </Link>
+      <nav className={c.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </>
 );
 
 export default MainHeader;

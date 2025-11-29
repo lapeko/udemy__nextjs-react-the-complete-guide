91c2da90a94629dbb7060aaefd3942a764af5654
lapeko <lapeko88@gmail.com>
2025-11-29 22:54:40 +0100

    init second project

---
+++ second-app/app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css';
+import { ReactNode } from 'react';
+
+export const metadata = {
+  title: 'Next.js Page Routing & Rendering',
+  description: 'Learn how to route to different pages.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}

a6a1bd9f9770a952c82e96c320cc7d9f527e7f84
lapeko <lapeko88@gmail.com>
2025-11-29 17:23:59 +0100

    implement image picker logic

--- first-app/components/image-picker/index.tsx
+++ first-app/components/image-picker/index.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { FC, useRef } from "react";
+import { FC, useRef, useState } from "react";
 import c from "./style.module.css";
+import Image from "next/image";
 
 type Props = {
   label: string;
@@ -11,11 +12,33 @@ type Props = {
 const ImagePicker: FC<Props> = ({ label, name }) => {
   const inputRef = useRef<HTMLInputElement>(null)
   const handlePickClick = () => inputRef.current?.click()
+  const [pickedImage, setPickedImage] = useState<string>()
+
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+
+    if (!file) {
+      setPickedImage(undefined)
+      return
+    }
+
+    const fileReader = new FileReader()
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result as string)
+    }
+
+    fileReader.readAsDataURL(file)
+  }
 
   return (
     <div className={c.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={c.controls}>
+        <div className={c.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && <Image fill src={pickedImage} alt="Picked image" />}
+        </div>
         <input
           className={c.input}
           type="file"
@@ -23,6 +46,8 @@ const ImagePicker: FC<Props> = ({ label, name }) => {
           accept="image/png, image/jpeg"
           name={name}
           ref={inputRef}
+          onChange={onImageChange}
+          required
         />
         <button
           className={c.button}

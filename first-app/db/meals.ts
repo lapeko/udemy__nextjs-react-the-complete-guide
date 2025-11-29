7e40b05b7b7295e640b007f1a4a25bbe16dea165
lapeko <lapeko88@gmail.com>
2025-11-29 01:01:04 +0100

    implement not found page

--- first-app/db/meals.ts
+++ first-app/db/meals.ts
@@ -5,6 +5,5 @@ const db = sql("db/meals.db")
 
 export const getMeals = async () => {
   await new Promise(res => setTimeout(res, 2000))
-  throw new Error("Unexpected error")
   return db.prepare("SELECT * FROM meals").all() as Meal[]
 }

71d386c7b24c4f546ebe352231915c32b044715f
lapeko <lapeko88@gmail.com>
2025-12-01 23:52:26 +0100

    implement news/slug/image component

---
+++ second-app/app/news/[slug]/image/page.tsx
@@ -0,0 +1,17 @@
+import news from "@/app/mocks/news-data";
+import { notFound } from "next/navigation";
+
+export default async ({ params }: PageProps<"/news/[slug]/image">) => {
+  const { slug } = await params;
+  const item = news.find(n => n.slug === slug);
+
+  if (!item) {
+    notFound();
+  }
+
+  return (
+    <div className="fullscreen-image">
+      <img src={`/images/news/${item.image}`} alt={item.title} />
+    </div>
+  )
+}
--- second-app/app/news/[slug]/page.tsx
+++ second-app/app/news/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import news from '@/app/mocks/news-data';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 
 export default async function NewsDetailPage(props: PageProps<'/news/[slug]'>) {
@@ -10,7 +11,9 @@ export default async function NewsDetailPage(props: PageProps<'/news/[slug]'>) {
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${item.image}`} alt={item.title} />
+        <Link href={`/news/${slug}/image`}>
+          <img src={`/images/news/${item.image}`} alt={item.title} />
+        </Link>
         <h1>{item?.title}</h1>
         <time dateTime={item.date}>{item.date}</time>
       </header>

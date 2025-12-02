537ef35bc5cc5fc1eda6a906b223554880b46627
lapeko <lapeko88@gmail.com>
2025-12-02 20:04:25 +0100

    use server side data fetching

--- second-app/app/(content)/news/page.tsx
+++ second-app/app/(content)/news/page.tsx
@@ -1,37 +1,14 @@
-'use client'
-
 import NewsList from '@/app/components/news-list';
-import { ReactNode, useEffect, useState } from 'react';
 import { NewsItem } from "@/pkg/domain/entity"
 
-export default function NewsPage() {
-  const [news, setNews] = useState<NewsItem[]>([]);
-  const [pending, setPending] = useState(false);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    setPending(true);
-
-    fetch("http://localhost:8080/news")
-      .then(res => {
-        if (!res.ok) throw new Error(`HTTP request error. ${res.status}: ${res.statusText}`);
-        return res.json()
-      })
-      .then((news: NewsItem[]) => setNews(news))
-      .catch(e => setError(e instanceof Error ? e.message : String(e)))
-      .finally(() => setPending(false))
-  }, []);
-
-  let content: ReactNode;
-
-  if (error) content = <p>{error}</p>
-  else if (pending) content = <p>Pending data...</p>
-  else content = <NewsList news={news} />
+export default async function NewsPage() {
+  const news = await fetch("http://localhost:8080/news")
+    .then(res => res.json() as Promise<NewsItem[]>)
 
   return (
     <div>
       <h1>News</h1>
-      {content}
+      <NewsList news={news} />
     </div>
   );
 }

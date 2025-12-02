271f95f12535dd780a7f60dd85574a8977593b77
lapeko <lapeko88@gmail.com>
2025-12-02 14:18:46 +0100

    fetch data on a client from beckend

--- second-app/app/(content)/news/page.tsx
+++ second-app/app/(content)/news/page.tsx
@@ -1,11 +1,37 @@
-import news from '@/app/mocks/news-data';
+'use client'
+
 import NewsList from '@/app/components/news-list';
+import { ReactNode, useEffect, useState } from 'react';
+import { NewsItem } from "@/pkg/domain/entity"
 
 export default function NewsPage() {
+  const [news, setNews] = useState<NewsItem[]>([]);
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setPending(true);
+
+    fetch("http://localhost:8080/news")
+      .then(res => {
+        if (!res.ok) throw new Error(`HTTP request error. ${res.status}: ${res.statusText}`);
+        return res.json()
+      })
+      .then((news: NewsItem[]) => setNews(news))
+      .catch(e => setError(e instanceof Error ? e.message : String(e)))
+      .finally(() => setPending(false))
+  }, []);
+
+  let content: ReactNode;
+
+  if (error) content = <p>{error}</p>
+  else if (pending) content = <p>Pending data...</p>
+  else content = <NewsList news={news} />
+
   return (
     <div>
       <h1>News</h1>
-      <NewsList news={news} />
+      {content}
     </div>
   );
 }

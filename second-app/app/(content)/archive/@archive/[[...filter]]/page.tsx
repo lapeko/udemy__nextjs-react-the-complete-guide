d61261a6d702b17b036ff14d5518f24b4af3ba0d
lapeko <lapeko88@gmail.com>
2025-12-02 21:45:01 +0100

    granular data fetching with suspense

--- second-app/app/(content)/archive/@archive/[[...filter]]/page.tsx
+++ second-app/app/(content)/archive/@archive/[[...filter]]/page.tsx
@@ -8,6 +8,44 @@ import {
 } from "@/pkg/domain/repo/news";
 import { FullNewsItem } from "@/pkg/domain/entity";
 import Link from "next/link";
+import { Suspense } from "react";
+
+type DateProps = {
+  year?: string;
+  month?: string;
+}
+
+const Navigation = async ({ year, month }: DateProps) => {
+  if (month) return;
+
+  const prefix = year ? `/archive/${year}` : "/archive";
+  const links = await (year ? getAvailableNewsMonths(year) : getAvailableNewsYears());
+
+  return (
+    <ul>
+      {
+        links.map(link => <li key={link}>
+          <Link href={`${prefix}/${link}`}>{link}</Link>
+        </li>)
+      }
+    </ul>
+  )
+}
+
+const NewsContent = async ({ year, month }: DateProps) => {
+  let news: FullNewsItem[];
+  if (!year) {
+    news = await getNews();
+  } else if (!month) {
+    news = await getNewsForYear(year)
+  } else {
+    news = await getNewsForYearAndMonth(year, month)
+  }
+
+  return news.length
+    ? <NewsList news={news} />
+    : <p>No news found for the selected period.</p>
+}
 
 type Props = {
   params: Promise<{ filter?: string[] }>
@@ -15,38 +53,23 @@ type Props = {
 
 export default async ({ params }: Props) => {
   const { filter } = await params;
-
-  let news: FullNewsItem[] = [];
-  let links: string[] = [];
-  if (!filter) {
-    news = await getNews();
-    links = await getAvailableNewsYears();
-  } else if (filter.length === 1) {
-    news = await getNewsForYear(filter[0])
-    links = getAvailableNewsMonths(filter[0]);
-  } else if (filter.length === 2) {
-    news = await getNewsForYearAndMonth(filter[0], filter[1])
-  }
-
-  const newsContent = news.length
-    ? <NewsList news={news} />
-    : <p>No news found for the selected period.</p>
-
-  const prefix = filter?.length ? `/archive/${filter[0]}` : "/archive";
+  const [year, month] = filter || [];
 
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
-            {links.map(link => <li key={link}>
-              <Link href={`${prefix}/${link}`}>{link}</Link>
-            </li>)}
+            <Suspense fallback={<p>Navigation updating...</p>} >
+              <Navigation year={year} month={month} />
+            </Suspense>
           </ul>
         </nav>
       </header>
       <h1>News Page</h1>
-      {newsContent}
+      <Suspense fallback={<p>Loading news...</p>}>
+        <NewsContent year={year} month={month} />
+      </Suspense>
     </>
   )
 }
